1f249941186bb7c551ec116cc1289b017c83379d
Amit Masram <amitmasram831@gmail.com>
2023-11-22 21:00:15 +0530

    Day 1

---
+++ Day1/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		System.out.print((8 * 9) + "\n" + (2 * (100 + 200)));
+	}
+}
---
+++ Day1/src/day3.java
@@ -0,0 +1,7 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		System.out.print((8 * 9) + "\n" + (2 * (200 + 300)));
+	}
+}
